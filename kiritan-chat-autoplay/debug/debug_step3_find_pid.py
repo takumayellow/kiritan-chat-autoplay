1543d65d7c7018318d6121f191c1cc7923d5e406
takumayellow <takuman1709@gmail.com>
2025-08-08 02:39:43 +0900

    chore: フォルダ構造をフラット化して重複ファイルを整理

--- kiritan-chat-autoplay/debug/debug_step3_find_pid.py
+++
@@ -1,8 +0,0 @@
-# debug_step3_find_pid.py
-import psutil
-
-print("=== SeikaSay2.exe プロセス一覧 ===")
-for proc in psutil.process_iter(['pid','name','exe']):
-    info = proc.info
-    if info['name'] and 'SeikaSay2.exe' in info['name']:
-        print(f"PID={info['pid']}, name={info['name']}, exe={info['exe']}")
